18aeffdefeee3fad0e26f43c419c48743322bc22
JuanMolinaRey <Juanmolinarey7@gmail.com>
2024-06-17 11:06:07 +0200

    Saxon

--- src/main/java/org/example/saxon/Saxon.java
+++ src/main/java/org/example/saxon/Saxon.java
@@ -1,4 +1,36 @@
 package org.example.saxon;
 
-public class Saxon {
+import org.example.soldier.Soldier;
+
+public class Saxon extends Soldier {
+    private String msg;
+
+    public Saxon(int health, int strength) {
+        super(health, strength);
+    }
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    @Override
+    public int attack() {
+        return super.attack();
+    }
+
+    public void receiveDamage(int damage) {
+   health = health - damage;
+
+    if(health > 0){
+        setMsg("A Saxon has received" + damage + "points of damage");
+    }
+        else{
+            setMsg("A Saxon has died in combat");
+
+        }
+    }
 }
+
