b0189af3a725290d11f23814e2e29d31b9b7b6f2
JuanMolinaRey <Juanmolinarey7@gmail.com>
2024-06-17 10:21:42 +0200

    2de4

--- src/main/java/org/example/soldier/Soldier.java
+++ src/main/java/org/example/soldier/Soldier.java
@@ -1,4 +1,19 @@
 package org.example.soldier;
 
 public class Soldier {
+    public int health = 300;
+    public int strength = 150;
+
+    public Soldier(int health, int strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+
+    public int attack(){
+        return strength;
+    }
+
+    public void receiveDamage(int damage){
+        health = health - damage;
+    }
 }
---
+++ src/main/java/org/example/vikins/Viking.java
@@ -0,0 +1,42 @@
+package org.example.vikins;
+
+import org.example.soldier.Soldier;
+
+public class Viking extends Soldier {
+    public String name;
+    private String msg;
+
+    public Viking(String name, int health, int strength) {
+        super(health, strength);
+        this.name = name;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String battleCry(){
+        return "Odin Owns You All!";
+    }
+
+    @Override
+    public void receiveDamage(int damage) {
+        super.receiveDamage(damage);
+
+        if (health > 0) {
+            setMsg(name + "has received" + damage + "points of damage");
+        }
+        else  {
+            setMsg(name + "has died in combat");
+        }
+    }
+    public int attack() {
+        return super.attack();
+
+    }
+
+}
